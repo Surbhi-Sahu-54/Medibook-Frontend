29bce44e0aafddf581235f78c350b5fd700aade3
Surbhi Sahu <surbhisahu3075@gmail.com>
2026-05-15 19:33:04 +0530

    Initial frontend setup

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,jsx}'],
+      exclude: [
+        'src/main.jsx',
+        'src/test/**',
+        'src/**/*.test.{js,jsx}',
+      ],
+    },
+  }
+})
